d8b4e4fbeb4441ff96da69e4ad4f7856c47f5c66
Snownee <i@snownee.com>
2022-10-10 16:43:43 +0800

    fix: mixin

--- src/main/java/snownee/lychee/mixin/arsnouveau/LightningEntityMixin.java
+++ src/main/java/snownee/lychee/mixin/arsnouveau/LightningEntityMixin.java
@@ -2,6 +2,7 @@ package snownee.lychee.mixin.arsnouveau;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -10,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.hollingsworth.arsnouveau.common.entity.LightningEntity;
 
 import net.minecraft.world.entity.Entity;
@@ -22,13 +23,11 @@ import snownee.lychee.lightning_channeling.LightningChannelingRecipe;
 @Mixin(LightningEntity.class)
 public class LightningEntityMixin {
 
-	@Shadow
+	@Shadow(remap = false)
 	private int lightningState;
-	@Shadow
+	@Shadow(remap = false)
 	private boolean effectOnly;
-	@Shadow
-	private List<Integer> hitEntities;
-	private final List<Entity> lychee$hitItems = Lists.newArrayList();
+	private final Set<Entity> lychee$hitItems = Sets.newHashSet();
 
 	@Inject(at = @At("TAIL"), method = "tick")
 	private void lychee_tick(CallbackInfo ci) {
@@ -44,7 +43,7 @@ public class LightningEntityMixin {
 			), method = "tick", locals = LocalCapture.CAPTURE_FAILSOFT
 	)
 	private void lychee_onThunderHit(CallbackInfo ci, List<Entity> list, Iterator<Entity> var2, Entity entity) {
-		if (!RecipeTypes.LIGHTNING_CHANNELING.isEmpty() && entity instanceof ItemEntity && !hitEntities.contains(entity.getId())) {
+		if (!RecipeTypes.LIGHTNING_CHANNELING.isEmpty() && entity instanceof ItemEntity) {
 			lychee$hitItems.add(entity);
 		}
 	}
