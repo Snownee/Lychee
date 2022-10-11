b137635d2efbefcc8ea24b56d5d3b5e2bc78fecb
MikhailTapio <mikhailtapio13468@163.com>
2022-10-11 11:59:32 +0800

    feat: ars nouveau lightning compatibility

---
+++ src/main/java/snownee/lychee/mixin/arsnouveau/LightningEntityMixin.java
@@ -0,0 +1,52 @@
+package snownee.lychee.mixin.arsnouveau;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import com.google.common.collect.Lists;
+import com.hollingsworth.arsnouveau.common.entity.LightningEntity;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.item.ItemEntity;
+import snownee.lychee.RecipeTypes;
+import snownee.lychee.lightning_channeling.LightningChannelingRecipe;
+
+@Mixin(LightningEntity.class)
+public class LightningEntityMixin {
+
+	@Shadow
+	private int lightningState;
+	@Shadow
+	private boolean effectOnly;
+	@Shadow
+	private List<Integer> hitEntities;
+	private final List<Entity> lychee$hitItems = Lists.newArrayList();
+
+	@Inject(at = @At("TAIL"), method = "tick")
+	private void lychee_tick(CallbackInfo ci) {
+		if (!RecipeTypes.LIGHTNING_CHANNELING.isEmpty() && lightningState == 0 && !lychee$hitItems.isEmpty()) {
+			LightningBolt entity = (LightningBolt) (Object) this;
+			LightningChannelingRecipe.on(entity, lychee$hitItems);
+		}
+	}
+
+	@Inject(
+			at = @At(
+					value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;thunderHit(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/LightningBolt;)V"
+			), method = "tick", locals = LocalCapture.CAPTURE_FAILSOFT
+	)
+	private void lychee_onThunderHit(CallbackInfo ci, List<Entity> list, Iterator<Entity> var2, Entity entity) {
+		if (!RecipeTypes.LIGHTNING_CHANNELING.isEmpty() && entity instanceof ItemEntity && !hitEntities.contains(entity.getId())) {
+			lychee$hitItems.add(entity);
+		}
+	}
+
+}
