14544d6c9cf2bcb65f420b83cce1659a2d35c972
MikhailTapio <mikhailtapio13468@163.com>
2022-10-11 12:04:53 +0800

    feat: item cooldown

--- src/main/java/snownee/lychee/PostActionTypes.java
+++ src/main/java/snownee/lychee/PostActionTypes.java
@@ -2,6 +2,7 @@ package snownee.lychee;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fml.ModLoadingContext;
+import snownee.lychee.core.post.AddItemCooldown;
 import snownee.lychee.core.post.AnvilDamageChance;
 import snownee.lychee.core.post.DropItem;
 import snownee.lychee.core.post.DropXp;
@@ -29,6 +30,7 @@ public class PostActionTypes {
 	public static final PostActionType<RandomSelect> RANDOM = register("random", new RandomSelect.Type());
 	public static final PostActionType<Explode> EXPLODE = register("explode", new Explode.Type());
 	public static final PostActionType<Hurt> HURT = register("hurt", new Hurt.Type());
+	public static final PostActionType<AddItemCooldown> ADD_ITEM_COOLDOWN = register("add_item_cooldown", new AddItemCooldown.Type());
 
 	public static <T extends PostActionType<?>> T register(String name, T t) {
 		ModLoadingContext.get().setActiveContainer(null); // bypass Forge warning
