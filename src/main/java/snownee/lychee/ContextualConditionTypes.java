24aa28907e1b0d65dccdb9f660b61da7c47106d0
Snownee <i@snownee.com>
2022-11-26 19:25:34 +0800

    feat: new condition: direction check

--- src/main/java/snownee/lychee/ContextualConditionTypes.java
+++ src/main/java/snownee/lychee/ContextualConditionTypes.java
@@ -5,6 +5,7 @@ import net.minecraftforge.fml.ModLoadingContext;
 import snownee.lychee.core.contextual.And;
 import snownee.lychee.core.contextual.Chance;
 import snownee.lychee.core.contextual.ContextualConditionType;
+import snownee.lychee.core.contextual.DirectionCheck;
 import snownee.lychee.core.contextual.EntityHealth;
 import snownee.lychee.core.contextual.Execute;
 import snownee.lychee.core.contextual.FallDistance;
@@ -33,6 +34,7 @@ public final class ContextualConditionTypes {
 	public static final ContextualConditionType<FallDistance> FALL_DISTANCE = register("fall_distance", new FallDistance.Type());
 	public static final ContextualConditionType<EntityHealth> ENTITY_HEALTH = register("entity_health", new EntityHealth.Type());
 	public static final ContextualConditionType<IsSneaking> IS_SNEAKING = register("is_sneaking", new IsSneaking.Type());
+	public static final ContextualConditionType<DirectionCheck> DIRECTION = register("direction", new DirectionCheck.Type());
 
 	public static <T extends ContextualConditionType<?>> T register(String name, T t) {
 		ModLoadingContext.get().setActiveContainer(null); // bypass Forge warning
