f7c75bbd157d703a27d28226df6acb2c0c69dd03
Snownee <i@snownee.com>
2023-01-02 19:09:24 +0800

    fix: mixin

--- src/main/java/snownee/lychee/mixin/InventoryMenuAccess.java
+++ src/main/java/snownee/lychee/mixin/InventoryMenuAccess.java
@@ -4,14 +4,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.InventoryMenu;
 
 @Mixin(InventoryMenu.class)
 public interface InventoryMenuAccess {
-
-	@Accessor
-	ContainerLevelAccess getAccess();
 
 	@Accessor
 	Player getOwner();
