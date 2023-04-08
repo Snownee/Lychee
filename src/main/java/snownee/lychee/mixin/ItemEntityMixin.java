64ab4a317bb479d51ded654b73d5e0826df32ee9
Snownee <i@snownee.com>
2023-04-08 10:21:39 +0800

    fix: #43

--- src/main/java/snownee/lychee/mixin/ItemEntityMixin.java
+++ src/main/java/snownee/lychee/mixin/ItemEntityMixin.java
@@ -7,32 +7,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.ItemEntity;
 import snownee.lychee.LycheeTags;
-import snownee.lychee.RecipeTypes;
 import snownee.lychee.core.recipe.LycheeCounter;
-import snownee.lychee.item_burning.ItemBurningRecipe;
 
 @Mixin(ItemEntity.class)
 public class ItemEntityMixin implements LycheeCounter {
 
-	@Inject(
-			at = @At(
-					value = "INVOKE", target = "Lnet/minecraft/world/entity/item/ItemEntity;discard()V", shift = At.Shift.AFTER
-			), method = "hurt"
-	)
-	private void lychee_hurt(DamageSource pSource, float pAmount, CallbackInfoReturnable<Boolean> ci) {
-		if (RecipeTypes.ITEM_BURNING.isEmpty()) {
-			return;
-		}
-		Entity entity = (Entity) (Object) this;
-		if (!entity.isAlive() && entity.getType() == EntityType.ITEM && entity.isOnFire()) {
-			ItemBurningRecipe.on((ItemEntity) entity);
-		}
-	}
+	private ResourceLocation lychee$recipeId;
+	private int lychee$count;
 
 	@Inject(at = @At("HEAD"), method = "fireImmune", cancellable = true)
 	private void lychee_fireImmune(CallbackInfoReturnable<Boolean> ci) {
@@ -40,9 +23,6 @@ public class ItemEntityMixin implements LycheeCounter {
 			ci.setReturnValue(true);
 		}
 	}
-
-	private ResourceLocation lychee$recipeId;
-	private int lychee$count;
 
 	@Override
 	public void lychee$setRecipeId(@Nullable ResourceLocation id) {
---
+++ src/main/java/snownee/lychee/mixin/MixinPlugin.java
@@ -0,0 +1,55 @@
+package snownee.lychee.mixin;
+
+import java.util.List;
+import java.util.Set;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import net.minecraftforge.fml.loading.LoadingModList;
+
+public class MixinPlugin implements IMixinConfigPlugin {
+	private static boolean hasMod(String modid) {
+		return LoadingModList.get().getModFileById(modid) != null;
+	}
+
+	@Override
+	public void onLoad(String mixinPackage) {
+
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		if ("snownee.lychee.mixin.ItemEntityHurtMixin".equals(mixinClassName)) {
+			return !hasMod("itemphysic");
+		}
+		if ("snownee.lychee.mixin.itemphysic.ItemEntityHurtMixin".equals(mixinClassName)) {
+			return hasMod("itemphysic");
+		}
+		return true;
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+	}
+
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
+}
