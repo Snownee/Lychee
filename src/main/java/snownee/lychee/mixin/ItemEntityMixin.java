d56d270700e718f0aae21b99c6e149aa45f2826f
Snownee <i@snownee.com>
2023-04-17 22:33:34 +0800

    fix: #43

--- src/main/java/snownee/lychee/mixin/ItemEntityMixin.java
+++ src/main/java/snownee/lychee/mixin/ItemEntityMixin.java
@@ -3,37 +3,19 @@ package snownee.lychee.mixin;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.ItemEntity;
 import snownee.lychee.LycheeTags;
-import snownee.lychee.RecipeTypes;
 import snownee.lychee.core.recipe.LycheeCounter;
-import snownee.lychee.item_burning.ItemBurningRecipe;
 
 @Mixin(ItemEntity.class)
 public class ItemEntityMixin implements LycheeCounter {
 
-	@Inject(
-			at = @At(
-					value = "INVOKE", target = "Lnet/minecraft/world/entity/item/ItemEntity;discard()V", shift = Shift.AFTER
-			), method = "hurt"
-	)
-	private void lychee_hurt(DamageSource pSource, float pAmount, CallbackInfoReturnable<Boolean> ci) {
-		if (RecipeTypes.ITEM_BURNING.isEmpty()) {
-			return;
-		}
-		Entity entity = (Entity) (Object) this;
-		if (!entity.isAlive() && entity.getType() == EntityType.ITEM && entity.isOnFire()) {
-			ItemBurningRecipe.on((ItemEntity) entity);
-		}
-	}
+	private ResourceLocation lychee$recipeId;
+	private int lychee$count;
 
 	@Inject(at = @At("HEAD"), method = "fireImmune", cancellable = true)
 	private void lychee_fireImmune(CallbackInfoReturnable<Boolean> ci) {
@@ -41,9 +23,6 @@ public class ItemEntityMixin implements LycheeCounter {
 			ci.setReturnValue(true);
 		}
 	}
-
-	private ResourceLocation lychee$recipeId;
-	private int lychee$count;
 
 	@Override
 	public void lychee$setRecipeId(@Nullable ResourceLocation id) {
