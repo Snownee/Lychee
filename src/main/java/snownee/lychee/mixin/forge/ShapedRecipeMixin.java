c7bcc47605365a602e1bd84da7b0ffddd731bede
Snownee <i@snownee.com>
2023-05-22 14:30:20 +0800

    feat: allows assigning NBT to ItemStack or Ingredient from JSON

---
+++ src/main/java/snownee/lychee/mixin/forge/ShapedRecipeMixin.java
@@ -0,0 +1,29 @@
+package snownee.lychee.mixin.forge;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.google.gson.JsonObject;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.ShapedRecipe;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import snownee.lychee.util.LUtil;
+
+@Mixin(value = ShapedRecipe.class, priority = 2000)
+public class ShapedRecipeMixin {
+
+	@Inject(method = "itemStackFromJson", at = @At("HEAD"), cancellable = true)
+	private static void lychee$itemStackFromJson(JsonObject jsonObject, CallbackInfoReturnable<ItemStack> cir) {
+		if (!jsonObject.has("lychee:tag")) {
+			return;
+		}
+		CompoundTag tag = LUtil.jsonToTag(jsonObject.getAsJsonObject("lychee:tag"));
+		ItemStack itemStack = CraftingHelper.getItemStack(jsonObject, true, false);
+		itemStack.getOrCreateTag().merge(tag);
+		cir.setReturnValue(itemStack);
+	}
+}
