80176f75ae0315e3c506f17560537b9bdde48317
Snownee <i@snownee.com>
2023-07-06 14:13:33 +0800

    target KubeJS 6.1

--- src/main/java/snownee/lychee/compat/kubejs/LycheeKubeJSEvents.java
+++ src/main/java/snownee/lychee/compat/kubejs/LycheeKubeJSEvents.java
@@ -8,7 +8,7 @@ public interface LycheeKubeJSEvents {
 
 	EventGroup GROUP = EventGroup.of("LycheeEvents");
 
-	EventHandler CLICKED_INFO_BADGE = GROUP.client("clickedInfoBadge", () -> ClickedInfoBadgeEventJS.class).extra(Extra.ID).cancelable();
-	EventHandler CUSTOM_ACTION = GROUP.startup("customAction", () -> CustomActionEventJS.class).extra(Extra.STRING).cancelable();
-	EventHandler CUSTOM_CONDITION = GROUP.startup("customCondition", () -> CustomConditionEventJS.class).extra(Extra.STRING).cancelable();
+	EventHandler CLICKED_INFO_BADGE = GROUP.client("clickedInfoBadge", () -> ClickedInfoBadgeEventJS.class).extra(Extra.ID).hasResult();
+	EventHandler CUSTOM_ACTION = GROUP.startup("customAction", () -> CustomActionEventJS.class).extra(Extra.STRING).hasResult();
+	EventHandler CUSTOM_CONDITION = GROUP.startup("customCondition", () -> CustomConditionEventJS.class).extra(Extra.STRING).hasResult();
 }
