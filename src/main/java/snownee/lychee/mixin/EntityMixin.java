baa03feb4ad53e5ef33e075e7dcb807f4c883fba
Snownee <i@snownee.com>
2024-01-27 04:59:03 +0800

    chore: parity & disable transitive AWs

--- src/main/java/snownee/lychee/mixin/EntityMixin.java
+++ src/main/java/snownee/lychee/mixin/EntityMixin.java
@@ -1,20 +1,16 @@
 package snownee.lychee.mixin;
 
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.LightningBolt;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.entity.item.ItemEntity;
-
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
 import snownee.lychee.LycheeTags;
 import snownee.lychee.RecipeTypes;
 
