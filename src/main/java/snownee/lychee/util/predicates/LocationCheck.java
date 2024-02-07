629e98045b10fd3d2c4ef9317f8d3f4d05700356
Snownee <i@snownee.com>
2024-02-07 17:36:28 +0800

    no Optional

--- src/main/java/snownee/lychee/util/predicates/LocationCheck.java
+++ src/main/java/snownee/lychee/util/predicates/LocationCheck.java
@@ -1,14 +1,11 @@
 package snownee.lychee.util.predicates;
 
-import java.util.Optional;
-
 import org.jetbrains.annotations.NotNull;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.util.ExtraCodecs;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
@@ -20,32 +17,32 @@ import snownee.lychee.mixin.predicates.LocationCheckAccess;
  * LootItemCondition that checks the {@link LootContextParams.ORIGIN} position against a {@link LocationPredicate}
  * after applying an offset to the origin position.
  */
-public record LocationCheck(Optional<LocationPredicate> predicate, BlockPos offset) implements LootItemCondition {
+public record LocationCheck(LocationPredicate predicate, BlockPos offset) implements LootItemCondition {
 	public static final Codec<LocationCheck> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-			ExtraCodecs.strictOptionalField(LocationPredicate.CODEC, "predicate").forGetter(LocationCheck::predicate),
+			LocationPredicate.CODEC.fieldOf("predicate").forGetter(LocationCheck::predicate),
 			LocationCheckAccess.getOffsetCodec().forGetter(LocationCheck::offset)
 	).apply(instance, LocationCheck::new));
 
 	@Override
 	public @NotNull LootItemConditionType getType() {
-		return LootItemConditions.LOCATION_CHECK;
+		return LootItemConditions.LOCATION_CHECK; //FIXME
 	}
 
 	public boolean test(LootContext context) {
 		final var vec3 = context.getParamOrNull(LootContextParams.ORIGIN);
-		return vec3 != null && (this.predicate.isEmpty() || this.predicate.get().matches(
+		return vec3 != null && this.predicate.matches(
 				context.getLevel(),
-				vec3.x() + (double) this.offset.getX(),
-				vec3.y() + (double) this.offset.getY(),
-				vec3.z() + (double) this.offset.getZ()
-		));
+				vec3.x() + this.offset.getX(),
+				vec3.y() + this.offset.getY(),
+				vec3.z() + this.offset.getZ()
+		);
 	}
 
 	public static LootItemCondition.Builder checkLocation(LocationPredicate.Builder builder) {
-		return () -> new LocationCheck(Optional.of(builder.build()), BlockPos.ZERO);
+		return () -> new LocationCheck(builder.build(), BlockPos.ZERO);
 	}
 
 	public static LootItemCondition.Builder checkLocation(LocationPredicate.Builder builder, BlockPos offset) {
-		return () -> new LocationCheck(Optional.of(builder.build()), offset);
+		return () -> new LocationCheck(builder.build(), offset);
 	}
 }
