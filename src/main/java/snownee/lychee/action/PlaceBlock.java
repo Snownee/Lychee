ad386ca3bd726c1f2f5083d863c00431379844e3
SettingDust <settingdust@gmail.com>
2024-03-05 17:22:43 +0800

    feat(action): add `PlaceBlock`
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- src/main/java/snownee/lychee/action/PlaceBlock.java
+++ src/main/java/snownee/lychee/action/PlaceBlock.java
@@ -1,151 +1,75 @@
 package snownee.lychee.action;
 
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.jetbrains.annotations.Nullable;
 
-import com.google.gson.JsonObject;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import net.minecraft.advancements.critereon.BlockPredicate;
-import net.minecraft.advancements.critereon.NbtPredicate;
+import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 import net.minecraft.core.BlockPos;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.core.Vec3i;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseFireBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LevelEvent;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.gameevent.GameEvent;
-import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import snownee.lychee.LycheeLootContextParams;
-import snownee.lychee.core.LycheeRecipeContext;
-import snownee.lychee.core.def.BlockPredicateHelper;
-import snownee.lychee.mixin.BlockPredicateAccess;
-import snownee.lychee.mixin.NbtPredicateAccess;
-import snownee.lychee.mixin.StatePropertiesPredicateAccess;
-import snownee.lychee.recipes.block_crushing.BlockCrushingRecipe;
+import snownee.lychee.LycheeRegistries;
+import snownee.lychee.recipes.BlockCrushingRecipe;
 import snownee.lychee.util.CommonProxy;
 import snownee.lychee.util.action.PostAction;
+import snownee.lychee.util.action.PostActionCommonProperties;
 import snownee.lychee.util.action.PostActionType;
 import snownee.lychee.util.action.PostActionTypes;
+import snownee.lychee.util.context.LycheeContext;
+import snownee.lychee.util.context.LycheeContextKey;
+import snownee.lychee.util.predicates.BlockPredicateExtensions;
 import snownee.lychee.util.recipe.ILycheeRecipe;
 
-public class PlaceBlock extends PostAction {
+public class PlaceBlock implements PostAction {
+	private final PostActionCommonProperties commonProperties;
 
 	public final BlockPredicate block;
 	public final BlockPos offset;
 
-	public PlaceBlock(BlockPredicate block, BlockPos offset) {
+	public PlaceBlock(PostActionCommonProperties commonProperties, BlockPredicate block, BlockPos offset) {
+		this.commonProperties = commonProperties;
 		this.block = block;
 		this.offset = offset;
 	}
 
-	@Override
-	public PostActionType<?> getType() {
-		return PostActionTypes.PLACE;
-	}
-
-	@SuppressWarnings("rawtypes")
-	@Override
-	protected void apply(ILycheeRecipe recipe, LycheeRecipeContext ctx, int times) {
-		BlockPos pos = ctx.getParamOrNull(LycheeLootContextParams.BLOCK_POS);
-		if (pos == null) {
-			pos = BlockPos.containing(ctx.getParam(LootContextParams.ORIGIN));
-		}
-		pos = pos.offset(offset);
-		ServerLevel level = ctx.serverLevel();
-		BlockState oldState = level.getBlockState(pos);
-		BlockState state = getNewState(oldState);
-		if (state == null) {
-			return;
-		}
-		if (state.isAir()) {
-			destroyBlock(level, pos, false);
-			return;
-		}
-		if (recipe instanceof BlockCrushingRecipe && !oldState.isAir()) {
-			level.levelEvent(LevelEvent.PARTICLES_DESTROY_BLOCK, pos, Block.getId(oldState));
-		}
-		BlockPredicateAccess access = (BlockPredicateAccess) block;
-		if (getType() == PostActionTypes.PLACE) {
-			Set<String> properties = ((StatePropertiesPredicateAccess) access.getProperties()).getProperties()
-					.stream()
-					.map($ -> $.getName())
-					.collect(Collectors.toSet());
-			for (Map.Entry<Property<?>, Comparable<?>> entry : oldState.getValues().entrySet()) {
-				Property property = entry.getKey();
-				if (properties.contains(property.getName()) || !state.hasProperty(property)) {
-					continue;
-				}
-				state = state.setValue(property, (Comparable) entry.getValue());
-			}
-			if (state.hasProperty(BlockStateProperties.WATERLOGGED) &&
-					oldState.getFluidState().isSourceOfType(Fluids.WATER)) {
-				state = state.setValue(BlockStateProperties.WATERLOGGED, true);
-			}
-		}
-		if (!level.setBlockAndUpdate(pos, state)) {
-			return;
-		}
-
-		NbtPredicate nbtPredicate = access.getNbt();
-		if (getType() == PostActionTypes.PLACE && nbtPredicate != NbtPredicate.ANY) {
-			BlockEntity blockentity = level.getBlockEntity(pos);
-			if (blockentity != null) {
-				if (blockentity.onlyOpCanSetNbt()) {
-					return;
-				}
-
-				CompoundTag compoundtag1 = blockentity.saveWithoutMetadata();
-				CompoundTag compoundtag2 = compoundtag1.copy();
-				compoundtag1.merge(((NbtPredicateAccess) nbtPredicate).getTag());
-				if (!compoundtag1.equals(compoundtag2)) {
-					blockentity.load(compoundtag1);
-					blockentity.setChanged();
-				}
-			}
-		}
-		level.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(state));
-	}
-
-	@Nullable
-	protected BlockState getNewState(BlockState oldState) {
-		return BlockPredicateHelper.anyBlockState(block);
-	}
-
 	private static boolean destroyBlock(Level level, BlockPos pos, boolean drop) {
-		BlockState blockstate = level.getBlockState(pos);
+		var blockstate = level.getBlockState(pos);
 		if (blockstate.isAir()) {
 			return false;
 		} else {
-			FluidState fluidstate = level.getFluidState(pos);
+			var fluidstate = level.getFluidState(pos);
 			if (!(blockstate.getBlock() instanceof BaseFireBlock)) {
 				level.levelEvent(LevelEvent.PARTICLES_DESTROY_BLOCK, pos, Block.getId(blockstate));
 			}
 
 			if (drop) {
-				BlockEntity blockentity = blockstate.hasBlockEntity() ? level.getBlockEntity(pos) : null;
+				var blockentity = blockstate.hasBlockEntity() ? level.getBlockEntity(pos) : null;
 				Block.dropResources(blockstate, level, pos, blockentity, null, ItemStack.EMPTY);
 			}
 
-			BlockState legacy = fluidstate.createLegacyBlock();
+			var legacy = fluidstate.createLegacyBlock();
 			if (legacy == blockstate) {
 				legacy = Blocks.AIR.defaultBlockState();
 			}
-			boolean flag = level.setBlock(pos, legacy, 3, 512);
+			var flag = level.setBlock(pos, legacy, 3, 512);
 			if (flag) {
 				level.gameEvent(null, GameEvent.BLOCK_DESTROY, pos);
 			}
@@ -155,9 +79,88 @@ public class PlaceBlock extends PostAction {
 	}
 
 	@Override
+	public PostActionCommonProperties commonProperties() {
+		return commonProperties;
+	}
+
+	@Override
+	public PostActionType<PlaceBlock> type() {
+		return PostActionTypes.PLACE;
+	}
+
+	@Override
+	public void apply(@Nullable ILycheeRecipe<?> recipe, LycheeContext context, int times) {
+		var lootParamsContext = context.get(LycheeContextKey.LOOT_PARAMS);
+		var blockPos = lootParamsContext.getOrNull(LycheeLootContextParams.BLOCK_POS);
+		if (blockPos == null) {
+			blockPos = BlockPos.containing(lootParamsContext.get(LootContextParams.ORIGIN));
+		}
+		blockPos = blockPos.offset(offset);
+		var level = context.get(LycheeContextKey.LEVEL);
+		var oldState = level.getBlockState(blockPos);
+		var state = getNewState(oldState);
+		if (state == null) {
+			return;
+		}
+		if (state.isAir()) {
+			destroyBlock(level, blockPos, false);
+			return;
+		}
+		if (recipe instanceof BlockCrushingRecipe && !oldState.isAir()) {
+			level.levelEvent(LevelEvent.PARTICLES_DESTROY_BLOCK, blockPos, Block.getId(oldState));
+		}
+
+		if (type() == PostActionTypes.PLACE) {
+			var properties = block.properties()
+					.map(StatePropertiesPredicate::properties)
+					.stream()
+					.flatMap(Collection::stream)
+					.map(StatePropertiesPredicate.PropertyMatcher::name)
+					.collect(Collectors.toSet());
+			for (var entry : oldState.getValues().entrySet()) {
+				var property = entry.getKey();
+				if (properties.contains(property.getName()) || !state.hasProperty(property)) {
+					continue;
+				}
+				state = state.setValue(property, (Comparable) entry.getValue());
+			}
+			if (state.hasProperty(BlockStateProperties.WATERLOGGED) && oldState.getFluidState().isSourceOfType(Fluids.WATER)) {
+				state = state.setValue(BlockStateProperties.WATERLOGGED, true);
+			}
+		}
+
+		if (!level.setBlockAndUpdate(blockPos, state)) {
+			return;
+		}
+
+		if (type() == PostActionTypes.PLACE && block.nbt().isPresent()) {
+			var blockEntity = level.getBlockEntity(blockPos);
+			if (blockEntity != null) {
+				if (blockEntity.onlyOpCanSetNbt()) {
+					return;
+				}
+
+				var prevTag = blockEntity.saveWithoutMetadata(level.registryAccess());
+				var originalTag = prevTag.copy();
+				prevTag.merge(block.nbt().get().tag());
+				if (!prevTag.equals(originalTag)) {
+					blockEntity.load(prevTag, level.registryAccess());
+					blockEntity.setChanged();
+				}
+			}
+		}
+		level.gameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Context.of(state));
+	}
+
+	@Nullable
+	protected BlockState getNewState(BlockState oldState) {
+		return BlockPredicateExtensions.anyBlockState(block);
+	}
+
+	@Override
 	public Component getDisplayName() {
-		BlockState state = BlockPredicateHelper.anyBlockState(block);
-		String key = CommonProxy.makeDescriptionId("postAction", PostActionTypes.PLACE.getRegistryName());
+		BlockState state = BlockPredicateExtensions.anyBlockState(block);
+		var key = CommonProxy.makeDescriptionId("postAction", LycheeRegistries.POST_ACTION.getKey(PostActionTypes.PLACE));
 		if (state.isAir()) {
 			return Component.translatable(key + ".consume");
 		}
@@ -166,7 +169,7 @@ public class PlaceBlock extends PostAction {
 
 	@Override
 	public List<ItemStack> getOutputItems() {
-		return BlockPredicateHelper.getMatchedItemStacks(block);
+		return BlockPredicateExtensions.matchedItemStacks(block);
 	}
 
 	@Override
@@ -179,39 +182,20 @@ public class PlaceBlock extends PostAction {
 		return false;
 	}
 
-	public static class Type extends PostActionType<PlaceBlock> {
+	public static class Type implements PostActionType<PlaceBlock> {
+		public static final Codec<PlaceBlock> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+				PostActionCommonProperties.MAP_CODEC.forGetter(PlaceBlock::commonProperties),
+				BlockPredicate.CODEC.fieldOf("block").forGetter(it -> it.block),
+				RecordCodecBuilder.<BlockPos>mapCodec(posInstance -> posInstance.group(
+						Codec.INT.fieldOf("offsetX").forGetter(Vec3i::getX),
+						Codec.INT.fieldOf("offsetY").forGetter(Vec3i::getY),
+						Codec.INT.fieldOf("offsetZ").forGetter(Vec3i::getZ)
+				).apply(posInstance, BlockPos::new)).forGetter(it -> it.offset)
+		).apply(instance, PlaceBlock::new));
 
 		@Override
-		public PlaceBlock fromJson(JsonObject o) {
-			return new PlaceBlock(BlockPredicateHelper.fromJson(o.get("block")), CommonProxy.parseOffset(o));
+		public Codec<PlaceBlock> codec() {
+			return CODEC;
 		}
-
-		@Override
-		public void toJson(PlaceBlock action, JsonObject o) {
-			BlockPos offset = action.offset;
-			if (offset.getX() != 0) {
-				o.addProperty("offsetX", offset.getX());
-			}
-			if (offset.getY() != 0) {
-				o.addProperty("offsetY", offset.getY());
-			}
-			if (offset.getZ() != 0) {
-				o.addProperty("offsetZ", offset.getX());
-			}
-			o.add("block", BlockPredicateHelper.toJson(action.block));
-		}
-
-		@Override
-		public PlaceBlock fromNetwork(FriendlyByteBuf buf) {
-			return new PlaceBlock(BlockPredicateHelper.fromNetwork(buf), buf.readBlockPos());
-		}
-
-		@Override
-		public void toNetwork(PlaceBlock action, FriendlyByteBuf buf) {
-			BlockPredicateHelper.toNetwork(action.block, buf);
-			buf.writeBlockPos(action.offset);
-		}
-
 	}
-
 }
