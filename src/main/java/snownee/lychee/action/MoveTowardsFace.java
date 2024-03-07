fb01c155a7bf4e668f49df02ed78bca85ab0f6b7
SettingDust <settingdust@gmail.com>
2024-03-07 12:11:41 +0800

    feat(action): add `MoveTowardsFace`
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- src/main/java/snownee/lychee/action/MoveTowardsFace.java
+++ src/main/java/snownee/lychee/action/MoveTowardsFace.java
@@ -1,46 +1,41 @@
 package snownee.lychee.action;
 
-import com.google.gson.JsonObject;
+import org.jetbrains.annotations.Nullable;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import io.netty.buffer.ByteBuf;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.util.GsonHelper;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.phys.Vec3;
 import snownee.lychee.LycheeLootContextParams;
-import snownee.lychee.core.LycheeRecipeContext;
 import snownee.lychee.util.action.PostAction;
+import snownee.lychee.util.action.PostActionCommonProperties;
 import snownee.lychee.util.action.PostActionType;
 import snownee.lychee.util.action.PostActionTypes;
+import snownee.lychee.util.context.LycheeContext;
+import snownee.lychee.util.context.LycheeContextKey;
 import snownee.lychee.util.recipe.ILycheeRecipe;
 
-public class MoveTowardsFace extends PostAction {
-
-	public final float factor;
-
-	public MoveTowardsFace(float factor) {
-		this.factor = factor;
-	}
+public record MoveTowardsFace(PostActionCommonProperties commonProperties, float factor) implements PostAction {
 
 	@Override
-	public PostActionType<?> getType() {
+	public PostActionType<MoveTowardsFace> type() {
 		return PostActionTypes.MOVE_TOWARDS_FACE;
 	}
 
 	@Override
-	public void doApply(ILycheeRecipe recipe, LycheeRecipeContext ctx, int times) {
-		apply(recipe, ctx, times);
-	}
-
-	@Override
-	protected void apply(ILycheeRecipe recipe, LycheeRecipeContext ctx, int times) {
-		BlockPos pos = ctx.getParamOrNull(LycheeLootContextParams.BLOCK_POS);
-		if (pos == null) {
-			pos = BlockPos.containing(ctx.getParam(LootContextParams.ORIGIN));
+	public void apply(@Nullable ILycheeRecipe<?> recipe, LycheeContext context, int times) {
+		var lootParamsContext = context.get(LycheeContextKey.LOOT_PARAMS);
+		var blockPos = lootParamsContext.getOrNull(LycheeLootContextParams.BLOCK_POS);
+		if (blockPos == null) {
+			blockPos = BlockPos.containing(lootParamsContext.get(LootContextParams.ORIGIN));
 		}
-		Vec3 vec = new Vec3(ctx.getParam(LycheeLootContextParams.DIRECTION).step()).scale(factor);
-		ctx.setParam(LootContextParams.ORIGIN, vec.add(Vec3.atCenterOf(pos)));
-		ctx.removeParam(LycheeLootContextParams.BLOCK_POS);
+		var vector = new Vec3(lootParamsContext.get(LycheeLootContextParams.DIRECTION).step()).scale(factor);
+		lootParamsContext.setParam(LootContextParams.ORIGIN, vector.add(Vec3.atCenterOf(blockPos)));
+		lootParamsContext.removeParam(LycheeLootContextParams.BLOCK_POS);
 	}
 
 	@Override
@@ -48,28 +43,20 @@ public class MoveTowardsFace extends PostAction {
 		return true;
 	}
 
-	public static class Type extends PostActionType<MoveTowardsFace> {
+	public static class Type implements PostActionType<MoveTowardsFace> {
+		public static final Codec<MoveTowardsFace> CODEC = RecordCodecBuilder.create(inst -> inst.group(
+				PostActionCommonProperties.MAP_CODEC.forGetter(MoveTowardsFace::commonProperties),
+				Codec.FLOAT.optionalFieldOf("factor", 1F).forGetter(MoveTowardsFace::factor)
+		).apply(inst, MoveTowardsFace::new));
 
 		@Override
-		public MoveTowardsFace fromJson(JsonObject o) {
-			return new MoveTowardsFace(GsonHelper.getAsFloat(o, "factor", 1));
-		}
-
-		@Override
-		public void toJson(MoveTowardsFace action, JsonObject o) {
-			o.addProperty("factor", action.factor);
-		}
-
-		@Override
-		public MoveTowardsFace fromNetwork(FriendlyByteBuf buf) {
+		public StreamCodec<? extends ByteBuf, MoveTowardsFace> streamCodec() {
 			throw new UnsupportedOperationException();
 		}
 
 		@Override
-		public void toNetwork(MoveTowardsFace action, FriendlyByteBuf buf) {
-			throw new UnsupportedOperationException();
+		public Codec<MoveTowardsFace> codec() {
+			return CODEC;
 		}
-
 	}
-
 }
