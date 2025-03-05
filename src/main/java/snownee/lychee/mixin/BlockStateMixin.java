fce6da5f2aaeb289db5abf406096cb8a89c55274
Snownee <i@snownee.com>
2025-03-05 14:18:23 +0800

    fix: do not use mixin extras

--- src/main/java/snownee/lychee/mixin/BlockStateMixin.java
+++ src/main/java/snownee/lychee/mixin/BlockStateMixin.java
@@ -3,12 +3,10 @@ package snownee.lychee.mixin;
 import java.util.function.Supplier;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
@@ -26,22 +24,27 @@ import snownee.lychee.random_block_ticking.RandomlyTickable;
 import snownee.lychee.util.CommonProxy;
 
 @Mixin(BlockStateBase.class)
-public class BlockStateMixin {
+public abstract class BlockStateMixin {
+	@Shadow
+	protected abstract BlockState asState();
+
+	@Shadow
+	private boolean isRandomlyTicking;
+
 	/**
 	 * Some implementation for {@link Block} overriding the `isRandomlyTicking`.
 	 */
-	@WrapOperation(
-			method = "initCache",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraft/world/level/block/Block;isRandomlyTicking(Lnet/minecraft/world/level/block/state/BlockState;)Z"))
-	private boolean initCache(Block block, BlockState blockState, Operation<Boolean> original) {
-		return original.call(block, blockState) || ((RandomlyTickable) block).lychee$isTickable(blockState);
+	@Inject(method = "initCache", at = @At("RETURN"))
+	private void initCache(CallbackInfo ci) {
+		BlockState state = asState();
+		if (((RandomlyTickable) state.getBlock()).lychee$isTickable(state)) {
+			isRandomlyTicking = true;
+		}
 	}
 
 	@Inject(at = @At("HEAD"), method = "randomTick", cancellable = true)
 	private void randomTick(ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource, CallbackInfo ci) {
-		BlockState state = (BlockState) (Object) this;
+		BlockState state = asState();
 
 		RandomlyTickable block = (RandomlyTickable) state.getBlock();
 		if (block.lychee$isTickable(state)) {
