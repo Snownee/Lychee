d1951de83e7049813412b7698eef008e4a70aaa1
SettingDust <settingdust@gmail.com>
2025-03-08 14:32:35 +0800

    fix: correct the item burning recipe times
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- src/main/java/snownee/lychee/recipes/ItemBurningRecipe.java
+++ src/main/java/snownee/lychee/recipes/ItemBurningRecipe.java
@@ -41,7 +41,7 @@ public class ItemBurningRecipe extends LycheeRecipe<LycheeContext> {
 		RecipeTypes.ITEM_BURNING.findFirst(context, entity.level()).ifPresent(it -> {
 			context.put(LycheeContextKey.RECIPE_ID, new RecipeContext(it.id()));
 			context.put(LycheeContextKey.RECIPE, it.value());
-			int times = it.value().getRandomRepeats(entity.getItem().getCount(), context);
+			int times = it.value().getRandomRepeats(entity.getItem().getCount() / it.value().input.count(), context);
 			var itemStackHolders = ItemStackHolderCollection.InWorld.of(entity);
 			context.put(LycheeContextKey.ITEM, itemStackHolders);
 			it.value().applyPostActions(context, times);
