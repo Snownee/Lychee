987d2547d4f85e2e2a0a1ed00ef30e1f0e092557
SettingDust <settingdust@gmail.com>
2025-03-10 20:00:23 +0800

    chore: add simple sprite element render
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- src/main/java/snownee/lychee/category/SpriteElementRenderer.java
+++ src/main/java/snownee/lychee/category/SpriteElementRenderer.java
@@ -5,13 +5,42 @@ import snownee.lychee.client.gui.RenderElement;
 
 public class SpriteElementRenderer extends RenderElement {
 	private final SpriteElement element;
+	private final int textureWidth;
+	private final int textureHeight;
+	private final int uPosition;
+	private final int vPosition;
+	private final int x;
+	private final int y;
+	private final int blitOffset;
+	private final int uWidth;
+	private final int vHeight;
 
-	public SpriteElementRenderer(SpriteElement element) {
+	public SpriteElementRenderer(
+			SpriteElement element,
+			int textureWidth,
+			int textureHeight,
+			int uPosition,
+			int vPosition,
+			int x,
+			int y,
+			int blitOffset,
+			int uWidth,
+			int vHeight
+	) {
 		this.element = element;
+		this.textureWidth = textureWidth;
+		this.textureHeight = textureHeight;
+		this.uPosition = uPosition;
+		this.vPosition = vPosition;
+		this.x = x;
+		this.y = y;
+		this.blitOffset = blitOffset;
+		this.uWidth = uWidth;
+		this.vHeight = vHeight;
 	}
 
 	@Override
 	public void render(GuiGraphics graphics) {
-//		graphics.blitSprite(element.id());
+		graphics.blitSprite(element.id(), textureWidth, textureHeight, uPosition, vPosition, x, y, blitOffset, uWidth, vHeight);
 	}
 }
--- src/main/java/snownee/lychee/util/category/UIElementType.java
+++ src/main/java/snownee/lychee/util/category/UIElementType.java
@@ -1,6 +1,16 @@
 package snownee.lychee.util.category;
 
+import net.minecraft.core.Registry;
+import snownee.lychee.Lychee;
+import snownee.lychee.LycheeRegistries;
+import snownee.lychee.category.SpriteElement;
 import snownee.lychee.util.SerializableType;
 
 public interface UIElementType<T extends UIElement> extends SerializableType<T> {
+	UIElementType<SpriteElement> SPRITE = register("sprite", new SpriteElement.Type());
+
+	private static <T extends UIElement> UIElementType<T> register(String name, UIElementType<T> type) {
+		Registry.register(LycheeRegistries.UI_ELEMENT, Lychee.id(name), type);
+		return type;
+	}
 }
