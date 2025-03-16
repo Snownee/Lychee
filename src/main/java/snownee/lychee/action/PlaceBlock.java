1706df6618a1b9b82d4d348e457fdd7fded5e067
SettingDust <settingdust@gmail.com>
2025-03-16 09:42:53 +0800

    fix: place block won't be hidden when placing air
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- src/main/java/snownee/lychee/action/PlaceBlock.java
+++ src/main/java/snownee/lychee/action/PlaceBlock.java
@@ -52,7 +52,12 @@ public final class PlaceBlock implements PostAction {
 		this.offset = offset;
 		this.commonProperties = new PostActionCommonProperties(
 				properties.conditions(), Optional.ofNullable(properties.icon()).or(() ->
-				(BlockPredicateExtensions.isAny(this.block) && this.offset.equals(BlockPos.ZERO)) ?
+				((this.offset.equals(BlockPos.ZERO)
+						&& BlockPredicateExtensions.isAny(this.block)
+						|| this.block.blocks()
+							.map(blocks -> blocks.stream().allMatch(it -> it.value() == Blocks.AIR))
+							.orElse(false))
+				) ?
 						Optional.of(PostActionCommonProperties.HIDDEN) :
 						Optional.empty()), properties.getPath()
 		);
