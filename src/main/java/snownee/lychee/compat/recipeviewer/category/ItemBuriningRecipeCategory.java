af8e31f12bfeebd947acdb058af4bf26a0f04ad1
SettingDust <settingdust@gmail.com>
2025-03-31 16:19:06 +0800

    fix(rv): correct item burning
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- src/main/java/snownee/lychee/compat/recipeviewer/category/ItemBuriningRecipeCategory.java
+++ src/main/java/snownee/lychee/compat/recipeviewer/category/ItemBuriningRecipeCategory.java
@@ -1,5 +1,7 @@
 package snownee.lychee.compat.recipeviewer.category;
 
+import org.joml.Vector2fc;
+
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -8,12 +10,14 @@ import snownee.lychee.recipes.ItemBurningRecipe;
 import snownee.lychee.util.VectorExtensions;
 
 public class ItemBuriningRecipeCategory extends ItemAndBlockCategory<ItemBurningRecipe> {
+	private static final Vector2fc METHOD_POSITION = VectorExtensions.offsetX(ItemAndBlockCategory.METHOD_POSITION, 15);
+
 	public ItemBuriningRecipeCategory(
 			RvCategoryType<ItemBurningRecipe> type,
 			ResourceLocation id,
 			RVHelper rvHandler
 	) {
-		super(type, id, rvHandler, INPUT_BLOCK_POSITION, VectorExtensions.withX(METHOD_POSITION, 27), INPUT_INGREDIENT_X);
+		super(type, id, rvHandler);
 	}
 
 	@Override
@@ -24,5 +28,10 @@ public class ItemBuriningRecipeCategory extends ItemAndBlockCategory<ItemBurning
 	@Override
 	protected boolean shouldRenderInputBlockTooltip(ItemBurningRecipe recipe) {
 		return false;
+	}
+
+	@Override
+	public Vector2fc methodPosition() {
+		return METHOD_POSITION;
 	}
 }
