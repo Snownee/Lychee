2610cacfb556965e390813b750237fca088c0039
SettingDust <settingdust@gmail.com>
2025-04-02 15:29:02 +0800

    fix(rv): remove input icon condition inverted
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- src/main/java/snownee/lychee/compat/recipeviewer/category/AbstractRvCategory.java
+++ src/main/java/snownee/lychee/compat/recipeviewer/category/AbstractRvCategory.java
@@ -43,7 +43,7 @@ public abstract class AbstractRvCategory<R extends ILycheeRecipe<LycheeContext>>
 
 	public static boolean needRemoveInputIcon(ILycheeRecipe<? extends LycheeContext> recipe) {
 		return recipe.postActions().stream()
-				.noneMatch(it -> it instanceof PlaceBlock placeBlock && placeBlock.hidden());
+				.anyMatch(it -> it instanceof PlaceBlock placeBlock && placeBlock.hidden());
 	}
 
 	public static RenderElement getRemoveInputIcon() {
