616b87841dea1f792c71b5c1c43ad84fad026a4a
Snownee <i@snownee.com>
2025-04-03 16:17:14 +0800

    feat: softer shadows

--- src/main/java/snownee/lychee/ui/SpriteElementRenderer.java
+++ src/main/java/snownee/lychee/ui/SpriteElementRenderer.java
@@ -2,6 +2,7 @@ package snownee.lychee.ui;
 
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
+import snownee.lychee.client.gui.LycheeGuiGraphics;
 import snownee.lychee.client.gui.RenderElement;
 
 public class SpriteElementRenderer extends RenderElement {
@@ -15,12 +16,14 @@ public class SpriteElementRenderer extends RenderElement {
 
 	@Override
 	public void render(GuiGraphics graphics) {
-		int width = (int) (this.width() * scale);
-		int height = (int) (this.height() * scale);
-		float xOff = (this.width() - width) / 2F;
-		int x = (int) (this.x() + xOff);
-		float yOff = (this.height() - height) / 2F;
-		int y = (int) (this.y() + yOff);
-		graphics.blitSprite(id, x, y, (int) z, width, height);
+		int width = (int) (width() * scale);
+		int height = (int) (height() * scale);
+		float xOff = (width() - width) / 2F;
+		int x = (int) (x() + xOff);
+		float yOff = (height() - height) / 2F;
+		int y = (int) (y() + yOff);
+		((LycheeGuiGraphics) graphics).lychee$setRenderType(LycheeGuiGraphics::guiTextured);
+		graphics.blitSprite(id, x, y, (int) z(), width, height);
+		((LycheeGuiGraphics) graphics).lychee$setRenderType(null);
 	}
 }
