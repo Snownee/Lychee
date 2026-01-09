5dcdfcec68f163cccbd971b613cd202d6608fdf6
SirCesarium <cesarium3600@gmail.com>
2026-01-09 18:13:12 -0400

    feat: add preserve enchantments and preserve attributes to AnvilCraftingRecipe

--- src/main/java/snownee/lychee/datagen/LycheeRecipeBuilder.java
+++ src/main/java/snownee/lychee/datagen/LycheeRecipeBuilder.java
@@ -226,6 +226,8 @@ public abstract class LycheeRecipeBuilder<T extends LycheeRecipeBuilder<T, R>, R
 		protected final int materialCost;
 		protected final ItemStack output;
 		protected final List<PostAction> assemblingActions = Lists.newArrayListWithExpectedSize(6);
+		protected boolean preserveEnchantments = true;
+		protected boolean preserveAttributes = true;
 
 		public AnvilCrafting(Ingredient left, @Nullable Ingredient right, int materialCost, int levelCost, ItemStack output) {
 			this.ingredients = right == null ? NonNullList.of(Ingredient.EMPTY, left) : NonNullList.of(Ingredient.EMPTY, left, right);
@@ -249,7 +251,7 @@ public abstract class LycheeRecipeBuilder<T extends LycheeRecipeBuilder<T, R>, R
 
 		@Override
 		public AnvilCraftingRecipe build() {
-			return new AnvilCraftingRecipe(properties(), ingredients, output, assemblingActions, levelCost, materialCost);
+			return new AnvilCraftingRecipe(properties(), ingredients, output, assemblingActions, levelCost, materialCost, preserveEnchantments, preserveAttributes);
 		}
 	}
 
