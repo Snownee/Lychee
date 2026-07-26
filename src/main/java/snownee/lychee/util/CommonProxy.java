ca3717b7434c54cf651b38958bdef650c372a3d4
Snownee <i@snownee.com>
2026-07-26 23:45:20 +0800

    .
    Signed-off-by: SettingDust <settingdust@gmail.com>
    
    # Conflicts:
    #	src/main/java/snownee/lychee/compat/recipe_api/VisualOnlyComponentsIngredient.java

--- src/main/java/snownee/lychee/util/CommonProxy.java
+++ src/main/java/snownee/lychee/util/CommonProxy.java
@@ -395,7 +395,6 @@ public class CommonProxy {
 		DripstoneParticleService.PARTICLE_TYPES.register(modEventBus);
 		Objects.requireNonNull(RecipeTypes.ALL);
 		Objects.requireNonNull(LycheeTags.FIRE_IMMUNE);
-		Objects.requireNonNull(LycheeRegistries.CONTEXTUAL);
 		Objects.requireNonNull(ContextualConditionType.AND);
 		Objects.requireNonNull(PostActionTypes.DROP_ITEM);
 		Objects.requireNonNull(RecipeSerializers.ALL);
--- src/main/java/snownee/lychee/util/action/PostAction.java
+++ src/main/java/snownee/lychee/util/action/PostAction.java
@@ -23,6 +23,7 @@ import net.minecraft.util.ExtraCodecs;
 import snownee.lychee.LycheeRegistries;
 import snownee.lychee.context.ActionContext;
 import snownee.lychee.util.CommonProxy;
+import snownee.lychee.util.codec.LycheeCodecs;
 import snownee.lychee.util.codec.LycheeParser;
 import snownee.lychee.util.context.LycheeContext;
 import snownee.lychee.util.contextual.Contextual;
@@ -32,9 +33,11 @@ import snownee.lychee.util.json.JsonPointer;
 import snownee.lychee.util.recipe.ILycheeRecipe;
 
 public interface PostAction extends PostActionDisplay, PostActionLike, ContextualPredicate, Contextual {
-	MapCodec<PostAction> MAP_CODEC = LycheeRegistries.POST_ACTION.byNameCodec()
-			.fieldOf("type")
-			.orElseGet(() -> PostActionTypes.IF)
+	MapCodec<PostAction> MAP_CODEC = LycheeCodecs.rename(
+					LycheeRegistries.POST_ACTION.byNameCodec()
+							.fieldOf("type")
+							.orElseGet(() -> PostActionTypes.IF),
+					"Field[PostActionType]")
 			.dispatchMap(PostAction::type, PostActionType::codec);
 	Codec<PostAction> OBJECT_CODEC = MAP_CODEC.codec();
 	Codec<PostAction> STRING_CODEC = new Codec<>() {
