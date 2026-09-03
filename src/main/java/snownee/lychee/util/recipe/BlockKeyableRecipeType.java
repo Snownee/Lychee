269078d47f4e2cc727ace0a494bbf03d22f5db63
Snownee <i@snownee.com>
2026-09-03 20:24:13 +0800

    feat: rework set_item behavior

--- src/main/java/snownee/lychee/util/recipe/BlockKeyableRecipeType.java
+++ src/main/java/snownee/lychee/util/recipe/BlockKeyableRecipeType.java
@@ -14,8 +14,11 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.protocol.game.ClientboundContainerSetSlotPacket;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -160,16 +163,33 @@ public class BlockKeyableRecipeType<R extends BlockKeyableRecipe> extends Lychee
 					times = recipe.getRandomRepeats(Math.max(1, times), context);
 					recipe.applyPostActions(context, times);
 					itemContext.postApply(!actionContext.avoidDefault, times);
-					player.setItemInHand(hand, context.getItem(0));
-					player.setItemInHand(
-							hand == InteractionHand.MAIN_HAND ? InteractionHand.OFF_HAND : InteractionHand.MAIN_HAND,
-							context.getItem(1)
-					);
+					if (player instanceof ServerPlayer serverPlayer) {
+						setItemInHand(serverPlayer, hand, context.getItem(0));
+						setItemInHand(
+								serverPlayer,
+								hand == InteractionHand.MAIN_HAND ? InteractionHand.OFF_HAND : InteractionHand.MAIN_HAND,
+								context.getItem(1));
+					}
 				}
 				return Optional.of(recipe);
 			}
 		}
 		return Optional.empty();
+	}
+
+	private void setItemInHand(ServerPlayer player, InteractionHand hand, ItemStack newItem) {
+		ItemStack oldItem = player.getItemInHand(hand);
+		if (!newItem.isEmpty() && !ItemStack.isSameItemSameComponents(oldItem, newItem)) {
+			// force set pop time
+			player.connection.send(new ClientboundContainerSetSlotPacket(
+					player.containerMenu.containerId,
+					player.containerMenu.incrementStateId(),
+					hand == InteractionHand.MAIN_HAND ?
+							InventoryMenu.USE_ROW_SLOT_START + player.getInventory().selected :
+							InventoryMenu.SHIELD_SLOT,
+					ItemStack.EMPTY));
+		}
+		player.setItemInHand(hand, newItem);
 	}
 
 	public boolean has(Block block) {
